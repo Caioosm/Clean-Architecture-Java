4585e234bf4537e736b3cd0f7f4adedc10f47e13
Caiooo <matossousacaio.cms@gmail.com>
2025-05-12 16:12:53 -0300

    init

---
+++ src/test/java/dev/java/_0/EventClean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java._0.EventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
